b1a59ee8eecb887865fe4ee9c8a7fbaefc5eaee6
Noorul Hasan Khan <NoorulHasan.Khan@hcl.com>
2023-12-25 04:44:33 +0530

    removed gorm.Model from models

--- cmd/database.go
+++ cmd/database.go
@@ -10,26 +10,38 @@ import (
 )
 
 type Track struct {
-	gorm.Model
-	TrackID string   `gorm:"primary_key;column:track_id;not null;ON CONFLICT DO NOTHING" binding:"-" json:"track_id,omitempty"`
+	// gorm.Model
+	ID      uint     `gorm:"primaryKey"`
+	TrackID string   `gorm:"column:track_id;uniqueIndex;not null;ON CONFLICT DO NOTHING" binding:"-" json:"track_id,omitempty"`
 	ISRC    string   `gorm:"column:isrc;uniqueIndex;not null" json:"isrc"`
 	Title   string   `gorm:"column:title;not null" json:"title,omitempty"`
 	Images  []Image  `gorm:"column:images;many2many:track_images" json:"images,omitempty"`
 	Artists []Artist `gorm:"column:artists;many2many:track_artists" json:"artists,omitempty"`
+	// CreatedAt time.Time `gorm:"coulmn:created_at;autoCreateTime" json:"-"`
+	// UpdatedAt time.Time `gorm:"coulmn:created_at;autoUpdateTime" json:"-"`
+	// DeletedAt time.Time `gorm:"coulmn:created_at;index" json:"-"`
 }
 
 type Image struct {
-	gorm.Model
+	// gorm.Model
+	ID     uint   `gorm:"primaryKey"`
 	Height int    `gorm:"column:height;not null" json:"height,omitempty"`
 	Width  int    `gorm:"column:width;not null" json:"width,omitempty"`
-	URL    string `gorm:"column:url;primary_key;not null;uniqueIndex;ON CONFLICT DO NOTHING" json:"url,omitempty"`
+	URL    string `gorm:"column:url;uniqueIndex;not null;ON CONFLICT DO NOTHING" json:"url,omitempty"`
+	// CreatedAt time.Time `gorm:"coulmn:created_at;autoCreateTime" json:"-"`
+	// UpdatedAt time.Time `gorm:"coulmn:created_at;autoUpdateTime" json:"-"`
+	// DeletedAt time.Time `gorm:"coulmn:created_at;index" json:"-"`
 }
 
 type Artist struct {
-	gorm.Model
-	ArtistID string `gorm:"column:artist_id;primary_key;not null;uniqueIndex;ON CONFLICT DO NOTHING" binding:"-" json:"artist_id,omitempty"`
+	// gorm.Model
+	ID       uint   `gorm:"primaryKey"`
+	ArtistID string `gorm:"column:artist_id;uniqueIndex;not null;ON CONFLICT DO NOTHING" binding:"-" json:"artist_id,omitempty"`
 	Name     string `gorm:"column:name;not null" json:"name,omitempty"`
 	URI      string `gorm:"column:uri;not null" json:"uri,omitempty"`
+	// CreatedAt time.Time `gorm:"coulmn:created_at;autoCreateTime" json:"-"`
+	// UpdatedAt time.Time `gorm:"coulmn:created_at;autoUpdateTime" json:"-"`
+	// DeletedAt time.Time `gorm:"coulmn:created_at;index" json:"-"`
 }
 
 var DB *gorm.DB
